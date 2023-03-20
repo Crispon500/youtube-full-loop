63880150b153f94032433b2d4303bccdf0951c35
Crispon500 <crispon500@gmail.com>
2023-03-20 12:30:31 -0500

    add playlist to loop and hide controls and title

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 // Load the YouTube iframe API
-var tag = document.createElement('script');
+const tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // Create a player object
 var player;
+
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
-    videoId: '7nnmvuC4NLc',
+    videoId: 'tPEE9ZwTmy0',
     playerVars: {
       'autoplay': 1,
-      'controls': 1,
+      'showinfo': 0,
+      'controls': 0,
+      playlist: 'tPEE9ZwTmy0,7nnmvuC4NLc',
       'loop': 1,
-      'mute': 1
+      'mute': 1,
     },
     events: {
       'onReady': onPlayerReady,
